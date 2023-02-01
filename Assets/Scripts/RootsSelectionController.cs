2873982be2dcf22a024f7a10d2c89c3880f597ce
Daniel Fróes <danielfroes15@gmail.com>
2023-02-01 14:28:58 -0300

    Basic level selection

---
+++ Assets/Scripts/RootsSelectionController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class RootsSelectionController : Singleton<RootsSelectionController>
+    {
+        [SerializeField] List<RootNodeRow> _rows = new();
+
+        int _currentRow = 0;
+
+        public void EnableNextRowSelection(int choosenNode)
+        {
+            _currentRow++;
+            if (_currentRow >= _rows.Count)
+            {
+                Debug.Log("End Game");
+                return;
+            }
+
+            _rows[_currentRow].Show(choosenNode);
+        }
+
+        private void Start()
+        {
+            _rows.ForEach(row => row.HideAll());
+            _rows[0].Show(0);
+        }
+
+
+        
+    }
+}
