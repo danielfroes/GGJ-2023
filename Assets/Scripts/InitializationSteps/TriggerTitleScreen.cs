7cefb513a7872a565978ccbdd60e52daf0d6ed88
Daniel Fróes <danielfroes15@gmail.com>
2023-02-24 13:14:02 -0300

    Implemented TitleScreen and fixed inverted canvas in gamescene

---
+++ Assets/Scripts/InitializationSteps/TriggerTitleScreen.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.Initialization;
+using Assets.Scripts.Screens;
+using Assets.Scripts.Utils;
+using System;
+
+namespace Assets.Scripts.InitializationSteps
+{
+    public class TriggerTitleScreen : IInitializationStep
+    {
+        public void Run()
+        {
+            ServiceLocator.Get<ScreenService>().Show<TitleScreen>();
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+    }
+}
---
+++ Assets/Scripts/Screens/TitleScreen.cs
@@ -0,0 +1,23 @@
+﻿
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Screens
+{
+    public class TitleScreen : AScreen
+    {
+        [SerializeField] Button _playButton;
+        [SerializeField] Button _quitButton;
+        [Scene][SerializeField] int _gameScene;
+        
+        
+        void Start()
+        {
+            _playButton.onClick.AddListener(() => SceneManager.LoadScene(_gameScene));
+            _quitButton.onClick.AddListener(() => Application.Quit());
+        }
+
+    }
+}
