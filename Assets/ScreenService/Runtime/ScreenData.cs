e90f427c4fb27d9dcdbbcf397591b29082a246c2
Daniel Fróes <danielfroes15@gmail.com>
2023-02-25 17:24:43 -0300

    mom_level_1 + fix in screen service + title screen

---
+++ Assets/ScreenService/Runtime/ScreenData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Assets.Game.Scripts.Utils;
+
+namespace Assets.Scripts.Screens
+{
+    [CreateAssetMenu(fileName = "Screen Data", menuName = "Screen Service/Screen Data")]
+    public class ScreenData : ScriptableObject
+    {
+        [SerializeReference][SerializeReferenceMenu] IScreenProvider _provider = new DefaultScreenProvider();
+
+        public IScreenProvider Provider => _provider;
+
+
+    }
+
+}
